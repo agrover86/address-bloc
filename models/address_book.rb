a71766c7b85155cc95b8176e99ab1aaedff67385
Amit Grover <amitgrover.asg@gmail.com>
2018-03-06 15:39:12 +0000

    assignment 91 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,4 +21,11 @@
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+   
+   def remove_entry(name, phone_number, email)
+     rmv_entry=Entry.new(name, phone_number, email) 
+     entries.delete_if { |entry| entry.name==rmv_entry.name && 
+     entry.phone_number==rmv_entry.phone_number && entry.email==rmv_entry.email} 
+   end
+   
  end
